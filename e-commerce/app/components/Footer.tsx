db6c085dc64e459f8edaf781e0f668e8b2bc4c0f
faahvor <tinafavour2018@gmail.com>
2024-09-29 00:18:09 +0100

    update

---
+++ e-commerce/app/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='flex justify-between px-5'>
+        <LuHome />
+       
+        
+    </div>
+  )
+}
+
+export default Footer
